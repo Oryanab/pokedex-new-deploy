13d0814528d4e1bb2a96f85ee0bbd01eb417da91
Oryanab <86202122+Oryanab@users.noreply.github.com>
2021-11-01 21:40:01 +0200

    deployment

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ const pokemonRouter = require("./routers/pokemonRouter");
 const userRouter = require("./routers/userRouter");
 const cors = require("cors");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(cors());
 app.use("/pokemon", pokemonRouter);
